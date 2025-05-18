29e1061f3d25ad9393968669f5b2519997dfdf3e
X E T R A <xetrazx@gmail.com>
2025-05-18 14:31:15 +0530

    Update install_one.sh

--- install_one.sh
+++ install_one.sh
@@ -55,7 +55,7 @@ arch-chroot /mnt /bin/bash <<EOF
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
